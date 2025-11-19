c175a8d1d7a9cc138290cdfa2ea47db40625689d
Guillermo López <willyaranda@gmail.com>
2025-11-20 00:43:21 +0100

    Actually removed google scripts set on the header

--- header.php
+++ header.php
@@ -17,34 +17,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="zoom-domain-verification" content="ZOOM_verify_KMhEiObFczwnG46wHdMKHq">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <!-- <script async="" src="//www.google-analytics.com/analytics.js"></script> -->
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<!-- 
-<script type="text/javascript">
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-48687607-1', 'arandadeduero.es');
-  ga('send', 'pageview');
-
-</script>
--->
-
-<!-- Google tag (gtag.js) -->
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-BXMC22W46S"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BXMC22W46S');
-</script>
--->
 
 <?php wp_body_open(); ?>
 <div id="page" class="site">
@@ -119,4 +95,3 @@
 		</div>
 	</div>
 </div>
-

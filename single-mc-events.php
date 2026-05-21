995175ee78e8b97f1dd70f59e805f4b095d63f34
Guillermo López <willyaranda@gmail.com>
2026-05-22 01:39:17 +0200

    Update single-mc-events.php

--- single-mc-events.php
+++ single-mc-events.php
@@ -26,12 +26,8 @@ get_header();
 
 						get_template_part( 'template-parts/content', get_post_type() );
 
-						the_post_navigation(
-							// array(
-							// 	'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'aranda-de-duero' ) . '</span> <span class="nav-title">%title</span>',
-							// 	'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'aranda-de-duero' ) . '</span> <span class="nav-title">%title</span>',
-							// )
-							array(
+					the_post_navigation(
+						array(
 								'prev_text' => '<span class="nav-subtitle">' . esc_html__( '', 'aranda-de-duero' ) . '</span> <span class="nav-title btn btn-primary mt-3">' . esc_html__("Noticia anterior") . '</span>',
 								'next_text' => '<span class="nav-subtitle">' . esc_html__( '', 'aranda-de-duero' ) . '</span> <span class="nav-title btn btn-primary mt-3">Noticia posterior</span>',
 							)
@@ -47,5 +43,4 @@ get_header();
 	</div>
 
 <?php
-// get_sidebar();
 get_footer();
